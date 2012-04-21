2164ef106418685284e128ea1bf8b38d5f484690
mmarionn <Administrator@WIN-G2IJ4LBN7BC.(none)>
2012-04-22 00:39:28 +0800

    My WotW UI

---
+++ addons/MUI/MUI.lua
@@ -0,0 +1,13 @@
+-------------------------------------------
+-- Initialization
+-------------------------------------------
+
+local addonName, nameSpace = ...
+
+nameSpace[1] = {}
+nameSpace[2] = {}
+nameSpace[3] = {}
+
+MUI = nameSpace
+
+local M, C, L = unpack(nameSpace)
